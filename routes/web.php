8a43a0db0a1dbacba9e020dd1f87bcf1fc0cf001
n67h <night67hunter@gmail.com>
2023-10-12 14:36:32 +0800

    Change main route

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\ProfileController;
+use App\Models\Note;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -15,7 +16,9 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    $note = Note::first();
+    return $note;
+    // return view('welcome');
 });
 
 Route::get('/dashboard', function () {
